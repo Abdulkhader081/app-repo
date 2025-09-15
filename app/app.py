a1f97aab9a69e53eb478d24489898b680aab672a
Abdulkhader081 <abdulkhadermd081@gmail.com>
2025-09-15 16:46:41 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,15 +8,18 @@ def hello():
 
 @app.route('/healthz')
 def healthz():
+    # Returning HTML content
     return """
     <html>
       <head>
         <title>Hello WORLD, This is from Flask App!</title>
       </head>
+      <body>
+        <h1>Status: Healthy</h1>
       </body>
     </html>
     """, 200
-    return jsonify({"status": "healthy", "message": "Application is running"}), 200
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
